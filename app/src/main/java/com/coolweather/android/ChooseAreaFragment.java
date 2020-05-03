a6e304ceac92c147d29b3460d966e07a98ccf176
zj719 <652201026@qq.com>
2020-05-03 14:29:36 +0000

    Add Bing background

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -72,6 +73,7 @@ public class ChooseAreaFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -82,6 +84,12 @@ public class ChooseAreaFragment extends Fragment {
                     selectedCity =cityList.get(position);
                     queryCounties();
 
+                }else if (currentLevel ==LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
